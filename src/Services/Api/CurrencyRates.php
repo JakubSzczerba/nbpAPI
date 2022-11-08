c4a81118523063c650dc65f5b25c274dd15adbde
gitname <gitemail>
2022-11-08 11:17:34 +0000

    move logic from controller to service

--- src/Services/Api/CurrencyRates.php
+++ src/Services/Api/CurrencyRates.php
@@ -1,8 +1,23 @@
 <?php
 
+/*
+ * This file was created by Jakub Szczerba
+ * Contact: https://www.linkedin.com/in/jakub-szczerba-3492751b4/
+*/
+
+declare(strict_types=1);
+
 namespace App\Services\Api;
+
+use GuzzleHttp\Client;
 
 class CurrencyRates
 {
+    public function integration(): Client
+    {
+        $client = new Client(['base_uri' => 'http://api.nbp.pl/api/exchangerates/tables/A/']);
+
+        return $client;
+    }
 
 }
